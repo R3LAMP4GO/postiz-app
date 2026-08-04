4c4241ace00e267b2c7ef245823953d2cca5116d
R3LAMP4GO <241341030+R3LAMP4GO@users.noreply.github.com>
2026-08-04 15:33:53 -0400

    Prevent dragging the navigation logo

--- apps/frontend/src/components/new-layout/logo.tsx
+++ apps/frontend/src/components/new-layout/logo.tsx
@@ -7,6 +7,7 @@ export const Logo = () => {
       alt="Douro Digital"
       width={60}
       height={60}
+      draggable={false}
       className="mt-[8px] min-w-[60px] min-h-[60px] object-contain"
     />
   );
